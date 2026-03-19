34e6843fc48a627902f0974675d42f167cd18680
Luke <luke@example.com>
2026-03-19 15:36:06 +0000

    maybe this time?

--- flake.nix
+++ flake.nix
@@ -1,17 +1,14 @@
 {
   description = "A template for Nix projects";
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self }: {
     # Define the templates here
-    templates.default = {
-      path = ./templates/default;
-      description = "A basic Nix project template with a hello world package";
-    };
  
     # You can also define other templates
-    templates.go = {
-      path = ./templates/go;
+    temlpates.go = {
+      path = ./go;
       description = "A Go project template with devShell";
     };
+
   };
 }
