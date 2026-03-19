007258954ac55112a3aaf790808b6c203b6ac2e9
Luke <luke@example.com>
2026-03-19 15:48:30 +0000

    add comment about set SHELL envar

--- go/flake.nix
+++ go/flake.nix
@@ -19,6 +19,10 @@
         gotools
         just
       ];
+      # shellHook sets the SHELL envar because there is a bug in Nixos
+        # that incorrectly loads a sub-shell, causing broken prompts.
+        # It should have been fixed int 25.11 but I am still running into it 
+        # see: https://github.com/NixOS/nix/issues/5873
       shellHook = ''
         export SHELL=${ pkgs.lib.getExe pkgs.bash }
         export PATH=$PATH:$GOPATH/bin
